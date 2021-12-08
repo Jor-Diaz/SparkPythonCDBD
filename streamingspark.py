fb548890927a9670e065d3ac46ed610373f6fb13
jorge <jorge.diazma@sansano.usm.cl>
2021-12-08 12:07:26 -0300

    init

--- streamingspark.py
+++ streamingspark.py
@@ -2,15 +2,16 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
 #Inicializar contexto
-sc = SparkContext("local[2]","NetWorkWordCount")
-ssc = StreamingContext(sc,1)
+sc = SparkContext("local[2]","NetworkWordCount")
+ssc = StreamingContext(sc,10)
 
 lines = ssc.socketTextStream("localhost",9090)
 
 words = lines.flatMap(lambda line: line.split(" "))
-pairs =words.map(lambda word: (word, 1))
-wordCounts = pairs.reduceByKey(lambda x, y: x+y)
-wordCounts.pprint()
+pairs = words.map(lambda word: (word, 1))
+wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+
+wordCounts.pprint() 
 
 ssc.start()
 ssc.awaitTermination()
