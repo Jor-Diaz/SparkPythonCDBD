379b8485be23793f1f60d15a73aaf92c97e82dea
jorge <jorge.diazma@sansano.usm.cl>
2021-12-08 14:43:23 -0300

    avance

--- streamingspark.py
+++ streamingspark.py
@@ -1,9 +1,11 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
+from .apriori import apriori
+
 
 #Inicializar contexto
 sc = SparkContext("local[2]","NetworkWordCount")
-ssc = StreamingContext(sc,5)
+ssc = StreamingContext(sc,20)
 
 lines = ssc.socketTextStream("localhost",9090)
 
@@ -11,11 +13,16 @@ lines = ssc.socketTextStream("localhost",9090)
 
 mayor=[]
 menor=[]
-words = lines.filter(lambda line: len(line)>9 )
-pairs = words.map(lambda word: (word, 1))
-wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+aux_particiones=[]
+#for i in range(1,19,2):
+#aux_particiones.append(lines.filter(lambda line: len(line)<i and len(line)>i-1))	 
 
-wordCounts.pprint() 
+#for i in aux_particiones:
+resultado=lines.reduce=apriori(i)
+#pairs = words.map(lambda word: (word, 1))
+#wordCounts = pairs.reduceByKey(lambda x, y: x + y) 
+
+resultado.pprint() 
 
 ssc.start()
 ssc.awaitTermination()
