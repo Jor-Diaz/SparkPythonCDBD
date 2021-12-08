4801b11274d53fe94f0e0ee8f0534fd4380a302d
jorge <jorge.diazma@sansano.usm.cl>
2021-12-08 17:10:43 -0300

    avance

--- dashboard/inicio/views.py
+++ dashboard/inicio/views.py
@@ -4,18 +4,24 @@ import json
 def inicio(request):
 	import os
 	context={}
-	aux = os.listdir('results')
+	aux = os.listdir('..\\..\\results')
 	context["data"]=aux
 	return render(request,'index.html',context)
 
 def ver(request):
 	import os
 	context={}
-	path="results/"+request.GET.get('u')
+	path="..\\..\\results\\"+request.GET.get('u')
 	print(path)
 	file = open(path,"r")
 	data = json.load(file)    
 	context["n_resultados"]=data["n_results"]
 	context["n_closures"]=data["n_closures"]
 	context["exec_time"]=data["exec_time"]
+	aux=data["data"]
+	lineas=[]
+	for i in aux:
+		aux1=i.replace("frozenset","")
+		lineas.append(aux1)
+	context["lineas"]=lineas		
 	return render(request,'ver.html',context)	
